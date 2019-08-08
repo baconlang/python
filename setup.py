20c76ad7929025864e365064010d18dc0b0521a4
Caleb Martinez <contact@calebmartinez.com>
2019-08-08 19:04:25 -0400

    Add proper description for PyPi and increment version

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='baclang',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['baclang'],
-    version='v1.0.1',
+    version='v1.0.2',
     license='MIT',
     description='Python implementation of the BACLang interpreter',
     author='Caleb Martinez',
     author_email='contact@calebmartinez.com',
     url='https://github.com/baclang/python',
-    download_url='https://github.com/baclang/python/archive/v1.0.1.tar.gz',
+    download_url='https://github.com/baclang/python/archive/v1.0.2.tar.gz',
     keywords=['language', 'interpreter', 'constraint', 'logic'],
     install_requires=[],
     classifiers=[
