372859d16370df58875238f37482a746cce63bf0
Caleb Martinez <contact@calebmartinez.com>
2019-08-09 03:38:05 -0400

    Remove description until I find out how to properly do it.

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from distutils.core import setup
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='baclang',
-    long_description=long_description,
     long_description_content_type='text/markdown',
     packages=['baclang'],
     version='v1.0.3',
